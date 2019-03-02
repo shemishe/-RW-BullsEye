80eadeeed4343106201a3f0a848287b7bc117bbb
Sherman Shi <shermanmishi@icloud.com>
2019-03-02 00:20:52 -0800

    Created basic layout, methods to find current value and target value, target label attached.

--- [RW] BullsEye/ViewController.swift
+++ [RW] BullsEye/ViewController.swift
@@ -9,12 +9,58 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var currentValue: Int = 0
+    var targetValue: Int = 0
+    
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var targetValueLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let roundedValue = slider.value.rounded()
+        currentValue = Int(roundedValue)
+        startNewRound()
+        
     }
+    
+    @IBAction func showAlert(_ sender: Any) {
+        
+        let alert = UIAlertController(title: "Hello World", message: "The value of the slider is: \(currentValue)." + "\nThe target value was: \(targetValue).", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Great!", style: .default, handler: nil)
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+        
+        startNewRound()
+        
+    }    
 
-
+    @IBAction func sliderMoved(_ sender: UISlider) {
+        
+        let roundedValue = sender.value.rounded()
+        
+        currentValue = Int(roundedValue)
+        
+    }
+    
+    func startNewRound() {
+        
+        targetValue = Int.random(in: 1...100)
+        currentValue = 50
+        slider.value = Float(currentValue)
+        updateLabels()
+        
+    }
+    
+    func updateLabels() {
+        
+        targetValueLabel.text = String(targetValue)
+        
+    }
+    
 }
 
